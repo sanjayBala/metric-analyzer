5b32d3e3cf4f4a4489247def3df8c26fb1f04cac
Sanjay Balaji <sanjay.balaji@oracle.com>
2020-09-04 20:46:48 +0530

    fix

--- main/json_parser.py
+++ main/json_parser.py
@@ -7,9 +7,38 @@ def testParser(json_object):
     #print_json(json_object)
     if  'check_suite' in json_object and json_object['action']=='completed':
         if json_object['check_suite']['conclusion']=='success':
-            check_suite_id = str(json_object['check_suite']['id'])
+            # repo metadata
+            repo_name = str(json_object['repository']['name'])
+            # CI metadata
+            check_suite_id = str(check_suite['id'])
+            check_suite = str(json_object['check_suite'])
+            timestamp = str(json_object['check_suite']['updated_at'])
+            conclusion = str(json_object['check_suite']['conclusion'])
             print("#####################################################################")
-            print("INFO CI-ID#" + check_suite_id + ": This looks like a valid deployment | Deployment Time: " + str(json_object['check_suite']['updated_at']))
+            print("INFO CI-ID#" + check_suite_id + ": This looks like a valid deployment | Deployment Time: " + timestamp)
+            deploy_point = {
+                "connectorType": "cloudockit",
+                "connectorId": "CDK",
+                "connectorVersion": "1.0.0",
+                "lxVersion": "1.0.0",
+                "lxWorkspace": "workspace-id",
+                "description": "Deployment Metric",
+                "processingDirection": "inbound",
+                "processingMode": "partial",
+                "content": [
+                    {
+                        "type": "DeployPoint",
+                        "id": repo_name,
+                        "data": {
+                            "status": conclusion,
+                            "timestamp": timestamp,
+                            "ciPipelineId": check_suite_id
+                        }
+                    }
+                ]
+            }
+            #json.dumps(deploy_point)
+            print_json(deploy_point)
             print("#####################################################################")
         else:
             print("#####################################################################")
