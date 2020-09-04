618531d2d37d81435b4e400d4df5cb4df497d824
Sanjay Balaji <sanjay.balaji@oracle.com>
2020-09-04 19:13:17 +0530

    pretty print

--- main/json_parser.py
+++ main/json_parser.py
@@ -8,6 +8,10 @@ def testParser(json_object):
     if  'check_suite' in json_object and json_object['action']=='completed':
         if json_object['check_suite']['conclusion']=='success':
             check_suite_id = str(json_object['check_suite']['id'])
+            print("#####################################################################")
             print("INFO ID#" + check_suite_id + ": This looks like a valid deployment | Deployment Time: " + str(json_object['check_suite']['updated_at']))
+            print("#####################################################################")
         else:
+            print("#####################################################################")
             print("INFO: This seems to be a failed deployment")
+            print("#####################################################################")
